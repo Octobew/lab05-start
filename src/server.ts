e7d198bcc8ce78e0de88512723734d1016f06089
APISARA ARUNWONG <apisara_ar@cmu.ac.th>
2026-01-11 16:47:11 +0700

    Task 4

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,41 @@
 import express, {Request, Response} from 'express'
 import eventRoute from "./routes/EventRoute";
+
+import multer from 'multer';
+import { uploadFile } from './services/UploadFileService';
+
 const app = express()
 const port = 3000
 app.use(express.json())
+
+const upload = multer({ storage: multer.memoryStorage() });
+app.post('/upload', upload.single('file'), async (req: any, res: any) => {
+  try {
+    const file = req.file;
+    if (!file) {
+      return res.status(400).send('No file uploaded.');
+    }
+
+    const bucket = 'lab5';
+    const filePath = `uploads/${file.originalname}`;
+
+    await uploadFile(bucket, filePath, file);
+
+    res.status(200).send('File uploaded successfully.');
+  } catch (error) {
+    res.status(500).send('Error uploading file.');
+  }
+});
+
+
+const corsOptions:CorsOptions = {
+   origin: ['http://localhost:5050'],
+   methods: ['GET','POST','OPTIONS'],
+   allowedHeaders: ['Content-Type','Authorization'],
+
+
+};
+app.use(cors(corsOptions))
 app.use('/events',eventRoute);
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
@@ -17,14 +50,7 @@ webApp.listen(webPort, () => {
 })
 
 import cors, {CorsOptions} from 'cors';
-const corsOptions:CorsOptions = {
-   origin: ['http://localhost:5050'],
-   methods: ['GET','POST','OPTIONS'],
-   allowedHeaders: ['Content-Type','Authorization'],
 
-
-};
-app.use(cors(corsOptions))
 
 
 
